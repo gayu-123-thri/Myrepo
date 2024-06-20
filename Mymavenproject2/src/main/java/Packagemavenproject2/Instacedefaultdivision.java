c06ae303fb3d6c95fc3f66cb2984f466078a4ab0
gayut <gayut@192.168.1.5>
2024-06-20 23:06:54 +0530

    commit1

---
+++ Mymavenproject2/src/main/java/Packagemavenproject2/Instacedefaultdivision.java
@@ -0,0 +1,19 @@
+package Packagemavenproject2;
+
+public class Instacedefaultdivision 
+{
+	int a=10;
+	int b=2;
+		public void div()
+		{
+		int c=a/b;
+			System.out.println("Division value is "  +c);
+		}
+
+	public static void main(String[] args) 
+	{
+		Instacedefaultdivision obj=new Instacedefaultdivision();
+		obj.div();
+	}
+
+}
---
+++ Mymavenproject2/src/main/java/Packagemavenproject2/Staticdefaultdivision.java
@@ -0,0 +1,19 @@
+package Packagemavenproject2;
+
+public class Staticdefaultdivision 
+{
+	static int a=10;
+	static int b=2;
+			public static void div()
+			{
+
+			int c=a/b;
+				System.out.println("Division value is "  +c);
+			}
+
+		public static void main(String[] args) 
+		{
+			Staticdefaultdivision .div();
+		}
+
+}
